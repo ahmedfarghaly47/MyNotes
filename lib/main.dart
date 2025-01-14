5c2ad1168a0832a482f69e42ce9e9270a4cd2b0f
Ahmed Farghaly <44965533+ahmedfarghaly47@users.noreply.github.com>
2025-01-14 09:20:38 +0200

    First View

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/views/notes_view.dart';
+
+void main() {
+  runApp(const MyNotes());
+}
+
+class MyNotes extends StatelessWidget {
+  const MyNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Notes',
+      theme: ThemeData(brightness: Brightness.dark),
+      home: NotesView(),
+    );
+  }
+}
