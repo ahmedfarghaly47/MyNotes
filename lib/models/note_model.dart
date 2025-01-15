8d2786669125eda2c0869765c23c23cb560cec21
Ahmed Farghaly <44965533+ahmedfarghaly47@users.noreply.github.com>
2025-01-15 08:24:21 +0200

    Create Hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,12 +1,19 @@
-class NoteModel {
-  final String id;
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String description;
+  @HiveField(2)
   final DateTime createdAt;
+  @HiveField(3)
   final int color;
 
   NoteModel({
-    required this.id,
     required this.title,
     required this.description,
     required this.createdAt,
